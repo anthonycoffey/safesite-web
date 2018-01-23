c0133cb0d4d7fb8fdb6c151062ed8fa3d774887f
David Muirhead <dm@oneiota.com>
2018-01-23 16:06:00 +1000

    build

--- library/custom-post-types.php
+++ library/custom-post-types.php
@@ -1,5 +1,7 @@
 <?php
-// Register Custom Post Type
+// Register Custom Post Types
+
+// Features Post type
 function Features() {
 
 	$labels = array(
@@ -54,4 +56,179 @@ function Features() {
 
 }
 add_action( 'init', 'Features', 0 );
+
+
+
+
+// Industry Post Type
+
+function industry() {
+
+	$labels = array(
+		'name'                  => _x( 'Industry Posts', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Industry', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Industry', 'text_domain' ),
+		'name_admin_bar'        => __( 'Industry', 'text_domain' ),
+		'archives'              => __( 'Industry Archives', 'text_domain' ),
+		'attributes'            => __( 'Industry Attributes', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+		'all_items'             => __( 'All Industry Posts', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Industry Post', 'text_domain' ),
+		'add_new'               => __( 'Add New', 'text_domain' ),
+		'new_item'              => __( 'New Item', 'text_domain' ),
+		'edit_item'             => __( 'Edit Item', 'text_domain' ),
+		'update_item'           => __( 'Update Item', 'text_domain' ),
+		'view_item'             => __( 'View Item', 'text_domain' ),
+		'view_items'            => __( 'View Items', 'text_domain' ),
+		'search_items'          => __( 'Search Item', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+		'items_list'            => __( 'Items list', 'text_domain' ),
+		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Industry', 'text_domain' ),
+		'description'           => __( 'Industry Posts', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor' ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'industry', $args );
+
+}
+add_action( 'init', 'industry', 0 );
+
+// Register Custom Post Type
+function resources_post_type() {
+
+	$labels = array(
+		'name'                  => _x( 'Resources', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Resource', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Resources', 'text_domain' ),
+		'name_admin_bar'        => __( 'Resources', 'text_domain' ),
+		'archives'              => __( 'Resource Archives', 'text_domain' ),
+		'attributes'            => __( 'Resource Attributes', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+		'all_items'             => __( 'All Resources', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Resources', 'text_domain' ),
+		'add_new'               => __( 'Add New', 'text_domain' ),
+		'new_item'              => __( 'New Item', 'text_domain' ),
+		'edit_item'             => __( 'Edit Item', 'text_domain' ),
+		'update_item'           => __( 'Update Item', 'text_domain' ),
+		'view_item'             => __( 'View Item', 'text_domain' ),
+		'view_items'            => __( 'View Items', 'text_domain' ),
+		'search_items'          => __( 'Search Item', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+		'items_list'            => __( 'Items list', 'text_domain' ),
+		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Resource', 'text_domain' ),
+		'description'           => __( 'Resources', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor' ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'resources', $args );
+
+
+// Register Company Post Type
+function company_post_type() {
+
+	$labels = array(
+		'name'                  => _x( 'Company Posts', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Company', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Company', 'text_domain' ),
+		'name_admin_bar'        => __( 'Company', 'text_domain' ),
+		'archives'              => __( 'Company Archives', 'text_domain' ),
+		'attributes'            => __( 'Company Post Attributes', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+		'all_items'             => __( 'Company Items', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Company Item', 'text_domain' ),
+		'add_new'               => __( 'Add New', 'text_domain' ),
+		'new_item'              => __( 'New Item', 'text_domain' ),
+		'edit_item'             => __( 'Edit Item', 'text_domain' ),
+		'update_item'           => __( 'Update Item', 'text_domain' ),
+		'view_item'             => __( 'View Item', 'text_domain' ),
+		'view_items'            => __( 'View Items', 'text_domain' ),
+		'search_items'          => __( 'Search Item', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+		'items_list'            => __( 'Items list', 'text_domain' ),
+		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Company', 'text_domain' ),
+		'description'           => __( 'Company posts', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor' ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'company', $args );
+
+}
+add_action( 'init', 'company_post_type', 0 );
+}
+add_action( 'init', 'resources_post_type', 0 );
+
+
+
 ?>
